a9f6427608202060804a9522492d21151bcf90e0
Honeyhavvi2012 <18so0072@odl.silveroaks.co.in>
2025-05-13 10:46:13 +0530

    Add files via upload

---
+++ Input.py
@@ -0,0 +1,9 @@
+i = int(input("enter a number:"))
+if(i<15):
+    print("i is smaller than 15")
+    print("i'm in if Block")
+elif(i < 50):
+    print("i is greater than 15")
+    print("i'm in else Block")
+else:
+    print("i'm not in if block and not elif")
